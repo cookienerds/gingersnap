9c15175c273381d0976fb41c7e198e742dce3421
Michael Calloo <admin@cookienerds.com>
2022-10-01 17:21:54 +0800

    fix Model annotator imports

--- src/annotations/index.ts
+++ src/annotations/index.ts
@@ -1,6 +1,6 @@
 export * from "./utils";
 export { Call, CallGroup, Callable } from "./core/Call";
-export { Model } from "./core/Model";
+export { Model, Ignore, Field, ArrayField } from "./core/Model";
 export * from "./core/Credentials";
 export { GingerSnap } from "./core";
 export { Service } from "./core/Service";
