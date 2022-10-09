00f6a6610ef1f4f34f9228b3623c3f07d7472b41
Michael Calloo <admin@cookienerds.com>
2022-10-09 08:02:34 +0800

    remove currying from plugins

--- src/utils/plugin.ts
+++ src/utils/plugin.ts
@@ -17,62 +17,65 @@ export interface ModelTagPropertyDescription {
 
 const modelClassTags: { [string: string]: ModelTagPropertyDescription } = {};
 
-export const createModelClassAnnotationTag = R.curry((name: string, properties: Object = {}) => (constructor: any) => {
-  modelClassTags[name] = {
-    properties,
-    modelClass: constructor.prototype,
-    modelClassName: constructor.name,
+export const createModelClassAnnotationTag =
+  (name: string, properties: Object = {}) =>
+  (constructor: any) => {
+    modelClassTags[name] = {
+      properties,
+      modelClass: constructor.prototype,
+      modelClassName: constructor.name,
+    };
   };
-});
 
-export const getModelClassAnnotationTagProperties = R.curry(
-  (tagNames: string[], ModelClass: any): ModelTagPropertyDescription[] => {
-    return R.map((tag) => modelClassTags[tag], tagNames).filter((v) => v);
-  }
-);
+export const getModelClassAnnotationTagProperties = (
+  tagNames: string[],
+  ModelClass: any
+): ModelTagPropertyDescription[] => {
+  return R.map((tag) => modelClassTags[tag], tagNames).filter((v) => v);
+};
 
-export const createModelFieldAnnotationTag = R.curry(
+export const createModelFieldAnnotationTag =
   (
-      name: string,
-      properties: Object = {},
-      onFieldCreated?: (tagName: string, properties: Object, target: Model, fieldName: string) => void
-    ) =>
-    (target: any, key: string) => {
-      const props: ModelInternalProps = namespacedModelInternalProps[target.constructor.name] ?? { fields: {} };
-      const result = R.find(([k, v]) => v.name === key, R.toPairs(props.fields));
+    name: string,
+    properties: Object = {},
+    onFieldCreated?: (tagName: string, properties: Object, target: Model, fieldName: string) => void
+  ) =>
+  (target: any, key: string) => {
+    const props: ModelInternalProps = namespacedModelInternalProps[target.constructor.name] ?? { fields: {} };
+    const result = R.find(([k, v]) => v.name === key, R.toPairs(props.fields));
 
-      if (!result) {
-        throw new Error(`Cannot create annotation tag of type ${name}, field does not exist as yet`);
-      }
-
-      props.fields[result[0]].customTags = props.fields[result[0]].customTags ?? {};
-      props.fields[result[0]].customTags![name] = {
-        __callback__: onFieldCreated,
-        properties,
-      };
-
-      props.parent = Object.getPrototypeOf(target).constructor.name;
-      namespacedModelInternalProps[target.constructor.name] = props;
+    if (!result) {
+      throw new Error(`Cannot create annotation tag of type ${name}, field does not exist as yet`);
     }
-);
 
-export const getModelFieldAnnotationTagProperties = R.curry(
-  (tagNames: string[], ModelClass: any): FieldTagPropertyDescription[] => {
-    const props: ModelInternalProps = (Model as any).buildPropTree(ModelClass.name);
-    return R.map(([k, v]) => {
-      if (v.customTags) {
-        const tagName = tagNames.find((t) => t in v.customTags!);
-        return tagName
-          ? {
-              tagName,
-              fieldName: v.name,
-              Type: v.Type,
-              isArray: v.isArray,
-              tagProperties: v.customTags[tagName].properties,
-            }
-          : null;
-      }
-      return null;
-    }, R.toPairs(props.fields)).filter((v) => v) as FieldTagPropertyDescription[];
-  }
-);
+    props.fields[result[0]].customTags = props.fields[result[0]].customTags ?? {};
+    props.fields[result[0]].customTags![name] = {
+      __callback__: onFieldCreated,
+      properties,
+    };
+
+    props.parent = Object.getPrototypeOf(target).constructor.name;
+    namespacedModelInternalProps[target.constructor.name] = props;
+  };
+
+export const getModelFieldAnnotationTagProperties = (
+  tagNames: string[],
+  ModelClass: any
+): FieldTagPropertyDescription[] => {
+  const props: ModelInternalProps = (Model as any).buildPropTree(ModelClass.name);
+  return R.map(([k, v]) => {
+    if (v.customTags) {
+      const tagName = tagNames.find((t) => t in v.customTags!);
+      return tagName
+        ? {
+            tagName,
+            fieldName: v.name,
+            Type: v.Type,
+            isArray: v.isArray,
+            tagProperties: v.customTags[tagName].properties,
+          }
+        : null;
+    }
+    return null;
+  }, R.toPairs(props.fields)).filter((v) => v) as FieldTagPropertyDescription[];
+};
