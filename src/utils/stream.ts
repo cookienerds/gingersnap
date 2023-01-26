fcd48a3d642fc5bd2d98f35eca454dd2218a7ec5
Michael Calloo <admin@cookienerds.com>
2023-01-26 19:47:46 +0800

    process nested executor state

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -445,17 +445,19 @@ export class Stream<T> implements AsyncGenerator<T> {
           data = actionData;
         } else if (this.canRunExecutor) {
           data = this.executor(this.controller.signal);
-          if (data instanceof ExecutorState) {
-            this.canRunExecutor = !data.done;
-            data = data.value;
-          }
-          if (data instanceof Promise) data = await data;
-          if (data instanceof Future) data = (await data).value;
-          if (data instanceof FutureResult) data = data.value;
-          if (data instanceof Stream) {
-            this.backlog.push({ actionIndex: 0, records: data });
-            return { state: State.CONTINUE };
-          }
+          do {
+            if (data instanceof ExecutorState) {
+              this.canRunExecutor = !data.done;
+              data = data.value;
+            }
+            if (data instanceof Promise) data = await data;
+            if (data instanceof Future) data = (await data).value;
+            if (data instanceof FutureResult) data = data.value;
+            if (data instanceof Stream) {
+              this.backlog.push({ actionIndex: 0, records: data });
+              return { state: State.CONTINUE };
+            }
+          } while (data instanceof ExecutorState);
         } else {
           return { state: State.DONE };
         }
